064e1d71bd9311286faecad493ab96af2bf2b493
chunyinlai1997 <prologic338@gmail.com>
2018-08-27 02:56:32 +0800

    assignment 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="#">
+                React Willon Lai
+              </NavbarBrand>
+            </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div >
     );
   }
 }
