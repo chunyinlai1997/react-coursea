7bb387919a77998472783f06ddf7fa7d61e4b3c8
chunyinlai1997 <prologic338@gmail.com>
2018-09-10 14:47:45 +0800

    React Redux Forms Revisited

---
+++ src/components/CommentComponent.js
@@ -0,0 +1,99 @@
+import React, {Component} from 'react';
+import {Button, Modal, ModalHeader, ModalBody, Row, Label} from 'reactstrap';
+import { Control, Form, Errors, actions } from 'react-redux-form';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => (val) && (val.length >= len);
+
+class CommentForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false,
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(values) {
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+        this.props.resetFeedbackForm();
+        // event.preventDefault();
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-sign-in fa-lg"></span> Submit Comment
+                </Button>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                         <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
+                            <Row className="form-group">
+                                <Label htmlFor="name">Rating</Label>
+                                <Control.select model=".rating" name="rating" className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="name">Your Name</Label>
+                                    <Control.text model=".author"
+                                                  id="author"
+                                                  name="author"
+                                                  placeholder="You Name"
+                                                  className="form-control"
+                                                  validators={{
+                                                      required,
+                                                      minLength: minLength(3),
+                                                      maxLength: maxLength(15)
+                                                  }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".name"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="name">Comment</Label>
+                                <Control.textarea model=".comment"
+                                                  id="comment"
+                                                  name="comment"
+                                                  rows="12"
+                                                  className="form-control"/>
+                            </Row>
+                            <Row className="form-group">
+                                <Button type="submit" color="primary">
+                                    Submit
+                                </Button>
+                            </Row>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default CommentForm;
